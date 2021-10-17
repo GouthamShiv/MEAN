04a64efe742c09d9c0281210fc11b2ce4f8dca7c
Goutham S <gouthamshine@gmail.com>
2021-10-18 02:50:55 +0530

    Optimizing & restructuring backend | udemy-MEAN-course | After 131st Lession | 18th October 2021 @02:50AM

--- udemy-MEAN-course/backend/src/controllers/post.controller.js
+++ udemy-MEAN-course/backend/src/controllers/post.controller.js
@@ -1,32 +1,6 @@
-const express = require('express');
-const multer = require('multer');
 const Post = require('../models/post.model');
-const checkAuth = require('../middleware/check-auth');
 
-const MIME_TYPE_MAP = {
-  'image/png': 'png',
-  'image/jpeg': 'jpg',
-  'image/jpg': 'jpg',
-};
-
-const router = express.Router();
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error('Invalid FileType');
-
-    if (isValid) error = null;
-
-    cb(error, './storage/images');
-  },
-  filename: (req, file, cb) => {
-    const name = file.originalname.toLowerCase().split(' ').join('_');
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + '-' + Date.now() + '.' + ext);
-  },
-});
-
-router.post('', checkAuth, multer({ storage: storage }).single('image'), (req, res, next) => {
+exports.createPost = (req, res, next) => {
   const url = req.protocol + '://' + req.get('host');
   const post = new Post({
     title: req.body.title,
@@ -58,9 +32,9 @@ router.post('', checkAuth, multer({ storage: storage }).single('image'), (req, r
     });
   // const post = req.body;
   // console.log(post);
-});
+};
 
-router.get('', (req, res, next) => {
+exports.getPosts = (req, res, next) => {
   const pageSize = +req.query.pageSize;
   const currentPage = +req.query.page;
   const postQuery = Post.find();
@@ -89,19 +63,43 @@ router.get('', (req, res, next) => {
         },
       });
     });
-});
+};
 
-router.delete('/:id', checkAuth, (req, res, next) => {
+exports.getPost = (req, res, next) => {
+  Post.findById(req.params.id)
+    .then(doc => {
+      if (!doc) {
+        res.status(404).json({
+          error: { message: 'Post not found' },
+        });
+      } else {
+        res.status(200).json({
+          message: 'Post retrieved successfuly',
+          posts: doc,
+        });
+      }
+    })
+    .catch(err => {
+      console.error(`unable to fetch records :: ${err}`);
+      res.status(500).json({
+        error: {
+          message: 'Unable to fetch post\nPlease retry',
+        },
+      });
+    });
+};
+
+exports.deletePost = (req, res, next) => {
   Post.deleteOne({ _id: req.params.id, creator: req.userData.userId })
     .then(result => {
-      if (result.n > 0) {
+      if (result.n <= 0) {
+        res.status(401).json({
+          error: { message: 'Not authorized to delete post' },
+        });
+      } else {
         console.log(`Post with ID ${req.params.id} was deleted`);
         res.status(200).json({
           message: 'Post deleted',
-        });
-      } else {
-        res.status(401).json({
-          error: { message: 'Not authorized to delete post' },
         });
       }
     })
@@ -113,9 +111,9 @@ router.delete('/:id', checkAuth, (req, res, next) => {
         },
       });
     });
-});
+};
 
-router.put('/:id', checkAuth, multer({ storage: storage }).single('image'), (req, res, next) => {
+exports.updatePost = (req, res, next) => {
   let imagePath = req.body.imagePath;
   if (req.file) {
     const url = req.protocol + '://' + req.get('host');
@@ -130,10 +128,10 @@ router.put('/:id', checkAuth, multer({ storage: storage }).single('image'), (req
   });
   Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post)
     .then(result => {
-      if (result.nModified > 0) {
-        res.status(200).json({ message: `Post with ID ${req.params.id} is updated successfully` });
-      } else {
+      if (result.nModified <= 0) {
         res.status(401).json({ error: { message: 'Not authorized to update the post' } });
+      } else {
+        res.status(200).json({ message: `Post with ID ${req.params.id} is updated successfully` });
       }
     })
     .catch(error => {
@@ -143,51 +141,4 @@ router.put('/:id', checkAuth, multer({ storage: storage }).single('image'), (req
         },
       });
     });
-});
-
-router.get('/:id', (req, res, next) => {
-  Post.findById(req.params.id)
-    .then(doc => {
-      if (doc) {
-        doc.creator = req.userData.userId;
-        res.status(200).json({
-          message: 'Post retrieved successfuly',
-          posts: doc,
-        });
-      } else {
-        res.status(404).json({
-          error: { message: 'Post not found' },
-        });
-      }
-    })
-    .catch(err => {
-      console.error(`unable to fetch records :: ${err}`);
-      res.status(500).json({
-        error: {
-          message: 'Unable to fetch post\nPlease retry',
-        },
-      });
-    });
-});
-
-// router.use('', (req, res, next) => {
-//   const posts = [
-//     {
-//       id: 'f0afw3423',
-//       title: 'Demo Post',
-//       content: 'Demo post retrieved from backend',
-//     },
-//     {
-//       id: 'f0afw8564',
-//       title: 'Another Post',
-//       content: 'Another demo post retrieved from backend',
-//     },
-//   ];
-//   res.status(200).json({
-//     message: 'Posts data',
-//     posts: posts,
-//   });
-//   // res.send('Hello from express!');
-// });
-
-module.exports = router;
+};
